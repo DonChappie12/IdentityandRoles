bea853740e752044d420e4df8cb931a5aefb4853
David Gonzalez <oneproductions12@gmail.com>
2019-04-08 17:11:01 -0700

    A few tweaks

--- Models/User.cs
+++ Models/User.cs
@@ -9,17 +9,17 @@ namespace thedashboard.Models
     public class User : IdentityUser
     {
         [Key]
-        // public int Id {get; set;}
-        // public string FirstName {get; set;}
-        // public string LastName {get; set;}
+        public int UserId {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
 
-        // [DataType(DataType.EmailAddress)]
-        // public string Email {get; set;}
+        [DataType(DataType.EmailAddress)]
+        public string Email {get; set;}
 
-        // [DataType(DataType.Password)]
-        // public string Password {get; set;}
+        [DataType(DataType.Password)]
+        public string Password {get; set;}
 
-        // public string Role {get; set;}
+        public string Role {get; set;}
 
         public List<Comments> Comment { get; set; }
         public List<Posts> Post { get; set; }
